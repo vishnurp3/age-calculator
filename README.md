e9e8445efd53eb3aa1eda27f5ab9fbe7c2f71539
Vishnu Prasad R <vishnurp3@gmail.com>
2024-06-15 20:42:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# Age Calculator
+
+This is a simple Age Calculator web application. Users can enter their date of birth and calculate their age in years,
+months, and days.
+
+## Features
+
+- Calculate age in years, months, and days
+- Prevent selection of future dates
+- Responsive design
+
+## Technologies Used
+
+- HTML
+- CSS
+- JavaScript
+- Flatpickr (for date picker)
+
+## Installation
+
+1. Clone the repository:
+   ```bash
+   git clone https://github.com/vishnurp3/age-calculator.git
+
+2. Open the index.html file in your web browser.
+
+## Usage
+
+1. Enter your date of birth.
+2. Click the "Calculate Age" button to see your age.
---
+++ index.js
@@ -0,0 +1,66 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const btnEl = document.getElementById("btn");
+    const birthdayEl = document.getElementById("birthday");
+    const resultEl = document.getElementById("result");
+
+    // Initialize Flatpickr
+    flatpickr(birthdayEl, {
+        dateFormat: "Y-m-d",
+        maxDate: "today",
+    });
+
+    const getAge = (birthdayValue) => {
+        const currentDate = new Date();
+        const birthdayDate = new Date(birthdayValue);
+        if (currentDate < birthdayDate) {
+            return {years: -1};
+        }
+
+        let years = currentDate.getFullYear() - birthdayDate.getFullYear();
+        let months = currentDate.getMonth() - birthdayDate.getMonth();
+        let days = currentDate.getDate() - birthdayDate.getDate();
+
+        if (days < 0) {
+            months--;
+            const lastMonth = new Date(currentDate.getFullYear(), currentDate.getMonth(), 0).getDate();
+            days += lastMonth;
+        }
+
+        if (months < 0) {
+            years--;
+            months += 12;
+        }
+
+        if (years === 0 && months === 0) {
+            return {days};
+        } else if (years === 0) {
+            return {months, days};
+        } else {
+            return {years, months, days};
+        }
+    };
+
+    const calculateAge = () => {
+        const birthdayValue = birthdayEl.value;
+        if (!birthdayValue) {
+            alert('Please enter your birthday');
+            return;
+        }
+
+        const age = getAge(birthdayValue);
+        if (age.years === -1) {
+            alert('The date cannot be in the future');
+            return;
+        }
+
+        if (age.years > 0) {
+            resultEl.innerText = `Your age is ${age.years} ${age.years > 1 ? "years" : "year"} old`;
+        } else if (age.months > 0) {
+            resultEl.innerText = `Your age is ${age.months} ${age.months > 1 ? "months" : "month"} old`;
+        } else {
+            resultEl.innerText = `Your age is ${age.days} ${age.days > 1 ? "days" : "day"} old`;
+        }
+    };
+
+    btnEl.addEventListener("click", calculateAge);
+});
